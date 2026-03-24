8895404a8a8050cbffe03f0eb8075976d9d9b6d8
Harish Kumar <jijeyharish@gmail.com>
2026-03-25 00:59:50 +0530

    version 5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dab_project',
-    version='0.0.4',
+    version='0.0.5',
     description='This contains the src code',
     author="Harish",
     packages=find_packages(where="src"),
